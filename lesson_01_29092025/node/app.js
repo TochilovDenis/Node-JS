4b1f4120cff76f26d2abac67fcb190ce9c7680d5
TochilovDenis <denis-tochilov@yandex.ru>
2025-09-29 19:19:30 +0300

    Первое приложение на Node.js
    https://metanit.com/web/nodejs/1.2.php

--- lesson_01_29092025/node/app.js
+++ lesson_01_29092025/node/app.js
@@ -1,1 +1,11 @@
 console.log("Здарова ☠");
+
+const http = require("http");
+http.createServer(function(request,response){
+     
+    response.end("Здарова ☠ NodeJS!");
+     
+}).listen(3000, "127.0.0.1",function(){
+    console.log("Сервер начал прослушивание запросов на порту 3000");
+});
+
