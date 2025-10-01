47e67f5dcee5a51e8e61d3ae1949879d4cc1d869
TochilovDenis <denis-tochilov@yandex.ru>
2025-10-01 20:57:44 +0300

    Запуск и обработка события
    для генерации события у EventEmitter надо вызвать метод emit(), в который передается имя события.
    
    emit(eventName [, args]): запускает событие. В качестве обязательного параметра принимает имя события. Также дополнительно можно передать необязательный параметр - аргументы для обработчика события
    
    https://metanit.com/web/nodejs/2.9.php

--- lesson_02_01102025/events/app.js
+++ lesson_02_01102025/events/app.js
@@ -1,3 +1,8 @@
 const EventEmitter = require("events");
 // определяем эмиттер событий
 const emitter = new EventEmitter();
+ 
+// имя события, которое будет обрабатываться
+const eventName = "greet";
+// генерируем событие greet
+emitter.emit(eventName);
