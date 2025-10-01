decf8d8b80ab7e5ea85af3c025e51fa512fce887
TochilovDenis <denis-tochilov@yandex.ru>
2025-10-01 21:01:48 +0300

    Запуск и обработка события
    Однако в ответ на это событие ничего не произойдет, событие пока никак не обрабатывается. Для регистрации обработчика события применяется метод on(), в который передается имя события и функция обработчика. Обработаем событие "greet".
    
    В качестве обработчика применяется функция, которая выводит строку "Hello World!" на консоль.
    
    https://metanit.com/web/nodejs/2.9.php

--- lesson_02_01102025/events/app.js
+++ lesson_02_01102025/events/app.js
@@ -4,5 +4,12 @@ const emitter = new EventEmitter();
  
 // имя события, которое будет обрабатываться
 const eventName = "greet";
+
+// регистрируем три обработчика для события "greet"
+emitter.on(eventName, function(){
+    console.log("Hello World!");
+});
+console.log("start emit")
+
 // генерируем событие greet
 emitter.emit(eventName);
