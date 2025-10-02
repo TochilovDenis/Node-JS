28bd6274c4e8c2d6ef601a87c01b1257d38981d8
TochilovDenis <denis-tochilov@yandex.ru>
2025-10-02 20:33:00 +0300

    практическая работа. Сделать так чтобы emitter.listenerCount(eventName) вывел значения: 15 9 1

--- lesson_03_02102025/task_01/app.js
+++ lesson_03_02102025/task_01/app.js
@@ -1,27 +1,14 @@
-const EventEmitter = require("events");
-// определяем эмиттер событий
+const { EventEmitter } = require('events');
 const emitter = new EventEmitter();
- 
-// Добавляем 15 обработчиков для события 'eventA'
-for (let i = 0; i < 15; i++) {
-    emitter.on('eventA', function() {
-        console.log(`eventA`);
-    });
-}
 
-// Добавляем 9 обработчиков для события 'eventB'
-for (let i = 0; i < 9; i++) {
-    emitter.on('eventB',  function() {
-        console.log(`eventB`);
-    });
-}
+// Создаем события с разным количеством слушателей
+for (let i = 0; i < 15; i++) emitter.on('event1', () => {});
+for (let i = 0; i < 9; i++) emitter.on('event2', () => {});
+emitter.on('event3', () => {});
 
-// Добавляем 1 обработчик для события 'eventC'
-emitter.on('eventC',  function() {
-    console.log('eventC');
-});
-
-// Проверяем количество обработчиков для каждого события
-console.log(emitter.listenerCount('eventA')); // Выведет: 15
-console.log(emitter.listenerCount('eventB')); // Выведет: 9
-console.log(emitter.listenerCount('eventC')); // Выведет: 1
+// Выводим все значения
+console.log(`
+    ${emitter.listenerCount('event1')} 
+    ${emitter.listenerCount('event2')} 
+    ${emitter.listenerCount('event3')}
+    `);
