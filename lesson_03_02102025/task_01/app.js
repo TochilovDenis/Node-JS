da287610fa2645add65e0bb688c53bff621ea097
TochilovDenis <denis-tochilov@yandex.ru>
2025-10-02 21:41:55 +0300

    2 задача использовать только один eventName  "console.log( emitter.listenerCount(eventName)"

--- lesson_03_02102025/task_01/app.js
+++ lesson_03_02102025/task_01/app.js
@@ -1,10 +1,33 @@
+/*1 практическая работа. Сделать так чтобы emitter.listenerCount(eventName) вывел значения:
+15
+9
+1*/
+
+/*
+2 задача использовать только один eventName  "console.log( emitter.listenerCount(eventName)"
+*/ 
+
 const { EventEmitter } = require('events');
 const emitter = new EventEmitter();
 
-// Создаем события с разным количеством слушателей
-for (let i = 0; i < 15; i++) emitter.on('event1', function(){});
-for (let i = 0; i < 9; i++) emitter.on('event2', function(){});
-emitter.on('event3', function(){});
+const eventName = 'event';
 
-// Выводим все значения
-console.log(`${emitter.listenerCount('event1')} ${emitter.listenerCount('event2')} ${emitter.listenerCount('event3')}`);
+// Добавляем слушатели, которые автоматически удаляются после первого вызова
+for (let i = 0; i < 15; i++) {
+    emitter.on(eventName, function(){});    
+}
+console.log(emitter.listenerCount(eventName)); // 15
+
+const listeners = emitter.listeners(eventName);
+for (let i = 0; i < 6; i++) {
+    emitter.removeListener(eventName, listeners[i]); // Удаляем 6 оставшихся слушателей, оставляя 9
+}
+console.log(emitter.listenerCount(eventName)); // Выводим 9 (15 - 6)
+
+const listeners1 = emitter.listeners(eventName);
+for (let i = 0; i < 8; i++) {
+    emitter.removeListener(eventName, listeners1[i]); // Удаляем 8 оставшихся слушателей, оставляя 1
+}
+
+// Выводим 1
+console.log(emitter.listenerCount(eventName));
