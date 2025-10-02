a06cda7659fcefa2b57dc9ea2f5eb650d64f2493
TochilovDenis <denis-tochilov@yandex.ru>
2025-10-02 20:34:51 +0300

    практическая работа. Сделать так чтобы emitter.listenerCount(eventName) вывел значения: 15 9 1

--- lesson_03_02102025/task_01/app.js
+++ lesson_03_02102025/task_01/app.js
@@ -2,13 +2,9 @@ const { EventEmitter } = require('events');
 const emitter = new EventEmitter();
 
 // Создаем события с разным количеством слушателей
-for (let i = 0; i < 15; i++) emitter.on('event1', () => {});
-for (let i = 0; i < 9; i++) emitter.on('event2', () => {});
-emitter.on('event3', () => {});
+for (let i = 0; i < 15; i++) emitter.on('event1', function(){});
+for (let i = 0; i < 9; i++) emitter.on('event2', function(){});
+emitter.on('event3', function(){});
 
 // Выводим все значения
-console.log(`
-    ${emitter.listenerCount('event1')} 
-    ${emitter.listenerCount('event2')} 
-    ${emitter.listenerCount('event3')}
-    `);
+console.log(`${emitter.listenerCount('event1')} ${emitter.listenerCount('event2')} ${emitter.listenerCount('event3')}`);
