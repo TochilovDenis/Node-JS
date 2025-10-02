e92e19550b0273f262c9e4966164e8d6fdd048db
TochilovDenis <denis-tochilov@yandex.ru>
2025-10-02 19:57:10 +0300

    Update

--- lesson_03_02102025/task_01/app.js
+++ lesson_03_02102025/task_01/app.js
@@ -2,26 +2,25 @@ const EventEmitter = require("events");
 // определяем эмиттер событий
 const emitter = new EventEmitter();
  
-// Добавляем 15 обработчиков для события 'eventA'
-for (let i = 0; i < 15; i++) {
-    emitter.on('eventA', () => {
-        console.log(`eventA`);
+const eventName = "greet";
+
+for(let i = 0; i < 15; i++){
+    emitter.on('eventA', function(){
+        console.log("15");
     });
 }
+console.log(emitter.listenerCount('eventA'));; // Выведет: 15
 
-// Добавляем 9 обработчиков для события 'eventB'
-for (let i = 0; i < 9; i++) {
-    emitter.on('eventB', () => {
-        console.log(`eventB`);
+for(let i = 0; i < 9; i++){
+    emitter.on('eventB', function(){
+        console.log("9");
     });
 }
+console.log(emitter.listenerCount('eventB'));; // Выведет: 9
 
-// Добавляем 1 обработчик для события 'eventC'
-emitter.on('eventC', () => {
-    console.log('eventC');
-});
-
-// Проверяем количество обработчиков для каждого события
-console.log(emitter.listenerCount('eventA')); // Выведет: 15
-console.log(emitter.listenerCount('eventB')); // Выведет: 9
-console.log(emitter.listenerCount('eventC')); // Выведет: 1
+for(let i = 0; i < 1; i++){
+    emitter.on('eventC', function(){
+        console.log("1");
+    });
+}
+console.log(emitter.listenerCount('eventC'));; // Выведет: 1
