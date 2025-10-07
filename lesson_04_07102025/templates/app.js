fa0a2aac65f2872c4218b54f9ba17e932a858ffa
TochilovDenis <denis-tochilov@yandex.ru>
2025-10-07 19:43:15 +0300

    Шаблоны
    https://metanit.com/web/nodejs/3.3.php

---
+++ lesson_04_07102025/templates/app.js
@@ -0,0 +1,21 @@
+const http = require("http");
+const fs = require("fs");
+  
+http.createServer(function(_, response){
+      
+    fs.readFile("index.html", function(error, data){
+                  
+        if(error) {
+            response.statusCode = 500;
+            response.end("Server error");
+        }
+        else{
+            const message = "Изучаем Node.js"; 
+            const header = "Главная страница";
+            const dataText = data.toString().replace(/{header}/g, header).replace(/{message}/g, message);
+            response.end(dataText);
+        }
+    })
+}).listen(3000, function(){
+    console.log("Сервер запущен по адресу http://localhost:3000");
+});
