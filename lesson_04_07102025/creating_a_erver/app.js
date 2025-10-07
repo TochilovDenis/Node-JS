2a7ed965105181b9c0488ed77c0bef42ba8b13eb
TochilovDenis <denis-tochilov@yandex.ru>
2025-10-07 19:11:11 +0300

    Request
    https://metanit.com/web/nodejs/3.1.php

--- lesson_04_07102025/creating_a_erver/app.js
+++ lesson_04_07102025/creating_a_erver/app.js
@@ -1,6 +1,12 @@
 const http = require("http");
  
-const server =  http.createServer(function(request, response){
+http.createServer(function(request, response){
+     
+    console.log("Url:", request.url);
+    console.log("Тип запроса:", request.method);
+    console.log("User-Agent:", request.headers["user-agent"]);
+    console.log("Все заголовки");
+    console.log(request.headers);
+     
     response.end("Hello METANIT.COM!");
-});
-server.listen(3000, function(){ console.log("Сервер запущен по адресу http://localhost:3000")});
+}).listen(3000, function(){ console.log("Сервер запущен по адресу http://localhost:3000")});
