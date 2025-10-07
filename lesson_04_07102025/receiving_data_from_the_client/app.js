fbdb8ea379fa26b0dd5783f77322ff978aae4594
TochilovDenis <denis-tochilov@yandex.ru>
2025-10-07 19:48:21 +0300

    Обработка событий data и end объекта request
    https://metanit.com/web/nodejs/3.2.php

---
+++ lesson_04_07102025/receiving_data_from_the_client/app.js
@@ -0,0 +1,20 @@
+const http = require("http");
+const fs = require("fs");
+ 
+http.createServer((request, response) => {
+      
+     if (request.url === "/user") {
+         
+        let data = "";
+        request.on("data", chunk => {
+            data += chunk;
+        });
+        request.on("end", () => {
+            console.log(data);
+            response.end("Данные успешно получены");
+        });
+    }
+    else{
+        fs.readFile("index.html", (error, data) => response.end(data));
+    }
+}).listen(3000, ()=>console.log("Сервер запущен по адресу http://localhost:3000"));
