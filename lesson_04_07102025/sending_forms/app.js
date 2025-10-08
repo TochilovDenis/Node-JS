a300801f312724ed2af0b9774bd0f4de7889638d
TochilovDenis <denis-tochilov@yandex.ru>
2025-10-08 20:16:16 +0300

    template

--- lesson_04_07102025/sending_forms/app.js
+++ lesson_04_07102025/sending_forms/app.js
@@ -1,32 +1,42 @@
 const http = require("http");
 const fs = require("fs");
-  
-http.createServer(async (request, response) => {
-    response.setHeader("Content-Type", "text/html; charset=utf-8;");   
 
-    if(request.url == "/user"){
-           
-          let body = "";   // буфер для получаемых данных
-         // получаем данные из запроса в буфер
-          for await (const chunk of request) {
-            body += chunk;
-          }
-        // для параметра name
-        let userName = "";
-        // для параметра age
-        let userAge = 0;
-        // разбиваем запрос на параметры по символу &
-        const params = body.split("&");
-        // проходим по всем параметрам и определяем их значения
-        for(param of params){
-            // разбиваем каждый параметр на имя и значение
-            const [paramName, paramValue] = param.split("=");
-            if(paramName === "username") userName = paramValue;
-            if(paramName === "userage") userAge = paramValue;
+http
+    .createServer(async (request, response) => {
+        if (request.url == "/user") {
+
+            let body = "";   // буфер для получаемых данных
+            // получаем данные из запроса в буфер
+            for await (const chunk of request) {
+                body += chunk;
+            }
+
+            // для параметра name
+            let fileName = "";
+
+            let temp = "<form action=\"user\" method=\"post\"><p><label>Название файла:</label><br><input name=\"file_name\" /></p><input type=\"submit\" value=\"Отправить\" /></form>"
+
+            // разбиваем запрос на параметры на имя и значение
+            const [paramName, paramValue] = body.split("=");
+
+            if (paramName === "file_name") fileName = paramValue;
+
+            //fs.createReadStream(fileName + '.html').pipe(response);
+
+            fs.readFile(fileName + ".html", function (error, data) {
+                if (error) {
+                    response.statusCode = 404;
+                    response.end("Resourse not found!");
+                }
+                else {
+                    response.end(data.toString().replace(/{temp}/g, temp));
+                }
+            });
         }
-        response.end(`Ваше имя: ${userName}  Твой возраст: ${userAge}`);
-    }
-    else{
-        fs.readFile("index.html", (_, data) => response.end(data));
-    }
-}).listen(3000, ()=>console.log("Сервер запущен по адресу http://localhost:3000"));
+        else {
+            fs.readFile("index.html", (_, data) => response.end(data));
+        }
+    })
+    .listen(3000, () => 
+        console.log("Сервер запущен по адресу http://localhost:3000")
+    );       
