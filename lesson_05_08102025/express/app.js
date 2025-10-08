a6d37970a3d12806901e9f64176d1174e3bf1c60
TochilovDenis <denis-tochilov@yandex.ru>
2025-10-08 20:28:47 +0300

    Три маршрута, которые будут обрабатываться сервером
    https://metanit.com/web/nodejs/4.1.php

--- lesson_05_08102025/express/app.js
+++ lesson_05_08102025/express/app.js
@@ -3,10 +3,17 @@ const express = require("express");
 // создаем объект приложения
 const app = express();
 // определяем обработчик для маршрута "/"
-app.get("/", function(request, response){
-     
-    // отправляем ответ
-    response.send("<h2>Привет Express!</h2>");
+app.get("/", function (request, response) {
+    response.send("<h1>Главная страница</h1>");
+});
+
+app.get("/about", function (request, response) {
+    response.send("<h1>О сайте</h1>");
+});
+
+app.get("/contact", function (request, response) {
+
+    response.send("<h1>Контакты</h1>");
 });
 // начинаем прослушивать подключения на 3000 порту
 app.listen(3000);
