4e027d3c3cf8d0b1486a8914dd5a3f646c5defb8
TochilovDenis <denis-tochilov@yandex.ru>
2025-10-08 20:43:15 +0300

    Пример middleware Middleware помогают выполнять некоторые задачи, которые должны быть сделаны до отправки ответа. Стандартная задача - логгирование запросов.
    https://metanit.com/web/nodejs/4.2.php

--- lesson_05_08102025/express/app.js
+++ lesson_05_08102025/express/app.js
@@ -1,23 +1,27 @@
 // подключение express
 const express = require("express");
+const fs = require("fs");
 // создаем объект приложения
 const app = express();
-app.use(function(_, _, next){
+app.use(function(request, response, next){
      
-    console.log("Middleware 1");
+    const now = new Date();
+    const hour = now.getHours();
+    const minutes = now.getMinutes();
+    const seconds = now.getSeconds();
+    const data = `${hour}:${minutes}:${seconds} ${request.method} ${request.url} ${request.get("user-agent")}`;
+    console.log(data);
+    fs.appendFile("server.log", data + "\n", function(error){
+        if(error) return console.log(error); // если возникла ошибка    
+        console.log("Запись файла завершена");
+    });
     next();
 });
 
-app.use("/about", function(_, response){
-     
-    console.log("About Middleware");
-    response.send("About Middleware");
-});
- 
-app.get("/", function(_, response){
-     
+app.get("/", function(_, response){ 
     console.log("Route /");
     response.send("Hello");
 });
+
 // начинаем прослушивать подключения на 3000 порту
 app.listen(3000);
