f09894c5d0c207f43424361a204097f759727142
TochilovDenis <denis-tochilov@yandex.ru>
2025-10-09 18:52:25 +0300

    express.static() передается путь к папке со статическими файлами. В данном случае мы указываем относительный путь - к папке "public" внутри текущей папки проекта. При необходимости также можно задать абсолютный путь.
     https://metanit.com/web/nodejs/4.4.php

--- lesson_05_08102025/express/app.js
+++ lesson_05_08102025/express/app.js
@@ -4,11 +4,10 @@ const fs = require("fs");
 // создаем объект приложения
 const app = express();
 
-app.use(express.static("public"));
-
+app.use(express.static(__dirname + "/public")); 
+ 
 
 app.use("/", function(_, response){
-     
     response.send("<h1>Главная страница</h1>");
 });
 
