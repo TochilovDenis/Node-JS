a1803d27ff7b255b997b4f652fe1cbea85ce98d1
TochilovDenis <denis-tochilov@yandex.ru>
2025-10-09 23:55:11 +0300

    Используя middleware и методы use получить в глобальные объекты параметры маршрута и параметры строки запроса . "//localhost:3000/name/gera/pet/murka.25?city=cher&obl=vol" в конце написать метод get app.get("/", function(_, response){     response.send(`Запрошенный данные: ${name},${age},${pet},${city},${obl}`); });

--- lesson_05_08102025/express/app.js
+++ lesson_05_08102025/express/app.js
@@ -4,21 +4,18 @@ const express = require("express");
 // создаем объект приложения
 const app = express();
 
-pageName = ""; pagePet = ""; pageAge = ""; pageCity = ""; pageObl = "";
-app.get("/name/:name/pet/:pet.:age", function (request, response) {
-  console.log(1)
+app.use("/name/:name/pet/:pet.:age", function (request, response,next) {
+  console.log("Middleware:", request.params, request.query);
   pageName = request.params["name"];
   pagePet = request.params["pet"];
   pageAge = request.params["age"];
-  pageCity = request.query.city;
-  pageObl = request.query.obl;
-  console.log(2)
-  response.redirect("/")
+  pageCity = request.query.city || "none";
+  pageObl = request.query.obl || "none";
+  next()
 });
 
-app.get("/", function (_, response) {
-  console.log(3)
-  response.send(`Запрошенный данные: ${pageName},${pageAge},${pagePet},${pageCity},${pageObl}`);
+app.get("/name/:name/pet/:pet.:age", function (_, response) {
+  response.send(`Запрошенный данные: ${pageName}, ${pageAge}, ${pagePet}, ${pageCity}, ${pageObl}`);
 });
 
 // начинаем прослушивать подключения на 3000 порту
