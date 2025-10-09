048467917efacdd16f434ffd42e619059e7905eb
TochilovDenis <denis-tochilov@yandex.ru>
2025-10-09 20:30:45 +0300

    Практическое задание.
    Используя параметры маршрута получить из запроса "//localhost:3000/name/gera/pet/murka.25"
    *имя/возраст/кличку питомца
    отправить данные пользователю(https://metanit.com/web/nodejs/4.15.php)

--- lesson_05_08102025/express/app.js
+++ lesson_05_08102025/express/app.js
@@ -1,20 +1,14 @@
 // подключение express
 const express = require("express");
-const fs = require("fs");
+
 // создаем объект приложения
 const app = express();
 
-app.get("/", function(_, response){
-      
-    response.send("<h1>Главная страница</h1>");
-});
-app.use("/about", function(request, response){
-      
-    const id = request.query.id;
-    const userName = request.query.name;
-    const userAge = request.query.age;
-    
-    response.send(`<h1>Информация</h1><p>id: ${id}</p><p>name: ${userName}</p></p><p>age: ${userAge}</p>`);
+app.get("/name/:nam/pet/:pet.:age", function (request, response) {
+    const pageName = request.params["name"];
+    const pagePet = request.params["pet"];
+    const pageAge = request.params["age"];
+     response.send(`Имя:${pageName} Питомец:${pagePet}.${pageAge}`);
 });
 
 // начинаем прослушивать подключения на 3000 порту
