6f08f152579a0d04c0a515e59a5e1b1346423163
TochilovDenis <denis-tochilov@yandex.ru>
2025-10-09 19:35:19 +0300

    Передача данных приложению. Параметры строки запроса.
    С помощью выражения request.query мы можем получить все параметры строки запрос в виде объекта javascript, а с помощью выражения request.query.название_параметра мы можем обратиться к каждому отдельному параметру.
    
    http://localhost:3000/about?id=1&name=Dex&age=35
    Информация
    id: 1
    name: Dex
    age: 35

--- lesson_05_08102025/express/app.js
+++ lesson_05_08102025/express/app.js
@@ -4,32 +4,17 @@ const fs = require("fs");
 // создаем объект приложения
 const app = express();
 
-//localhost:3000/aaa/bbb/ccc/eee/ggg/ttt/rrr/yyy
-app.get("/aaa/bbb/ccc/eee/ggg/ttt/rrr/yyy", function (_, response) {
-  response.redirect("../../../../../")
-  //localhost:3000/aaa/bbb
+app.get("/", function(_, response){
+      
+    response.send("<h1>Главная страница</h1>");
 });
-
-//localhost:3000/aaa/bbb
-app.get("/aaa/bbb", function (_, response) {
-  response.redirect("../../ggg/ttt/rrr/yyy")
-  //localhost:3000/ggg/ttt
-});
-
-//localhost:3000/ggg/ttt
-app.get("/ggg/ttt/rrr/yyy", function (_, response) {
-  response.redirect("./yyy/aaa/bbb/ccc")
-  //localhost:3000/ggg/ttt/rrr/yyy/aaa/bbb/ccc
-});
-
-//localhost:3000/ggg/ttt/rrr/yyy/aaa/bbb/ccc
-app.get("/ggg/ttt/rrr/yyy/aaa/bbb/ccc", function (_, response) {
-  response.redirect("/")
-  //localhost:3000/aaa/bbb/ccc
-});
-
-app.get("/", function (_, response) {
-  response.send("<h1>Hello</h1>");
+app.use("/about", function(request, response){
+      
+    const id = request.query.id;
+    const userName = request.query.name;
+    const userAge = request.query.age;
+    
+    response.send(`<h1>Информация</h1><p>id: ${id}</p><p>name: ${userName}</p></p><p>age: ${userAge}</p>`);
 });
 
 // начинаем прослушивать подключения на 3000 порту
