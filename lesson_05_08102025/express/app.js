53e98cd3ac8204c61c2eeedf0c2d6c81aff77da9
TochilovDenis <denis-tochilov@yandex.ru>
2025-10-09 18:39:08 +0300

    Статические файлы в Express
    https://metanit.com/web/nodejs/4.4.php

--- lesson_05_08102025/express/app.js
+++ lesson_05_08102025/express/app.js
@@ -3,28 +3,15 @@ const express = require("express");
 const fs = require("fs");
 // создаем объект приложения
 const app = express();
-app.use(function(request, response, next){
+
+app.use(express.static("public"));
+
+
+app.use("/", function(_, response){
      
-    const now = new Date();
-    const hour = now.getHours();
-    const minutes = now.getMinutes();
-    const seconds = now.getSeconds();
-    const data = `${hour}:${minutes}:${seconds} ${request.method} ${request.url} ${request.get("user-agent")}`;
-    console.log(data);
-    fs.appendFile("server.log", data + "\n", function(error){
-        if(error) return console.log(error); // если возникла ошибка    
-        console.log("Запись файла завершена");
-    });
-    next();
+    response.send("<h1>Главная страница</h1>");
 });
 
-// app.use(function (_, response) {
-//   response.sendFile(__dirname + "/index.html");
-// });
-
-app.use("/home/foo/bar",function (_, response) {
-  response.sendStatus(404)
-});
 
 // начинаем прослушивать подключения на 3000 порту
 app.listen(3000);
