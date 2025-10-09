ee4cbc6a274243c0657db1b77328c208dc2e4052
TochilovDenis <denis-tochilov@yandex.ru>
2025-10-10 00:00:03 +0300

    Практическое задание. 1 Используя параметры маршрута получить из запроса "//localhost:3000/name/gera/pet/murka.25" *имя/возраст/кличку питомца отправить данные пользователю(https://metanit.com/web/nodejs/4.15.php) 2 Используя middleware и методы use получить в глобальные объекты параметры маршрута и параметры строки запроса . "//localhost:3000/name/gera/pet/murka.25?city=cher&obl=vol" в конце написать метод get app.get("/", function(_, response){     response.send(`Запрошенный данные: ${name},${age},${pet},${city},${obl}`); });

--- lesson_05_08102025/express/app.js
+++ lesson_05_08102025/express/app.js
@@ -4,6 +4,15 @@ const express = require("express");
 // создаем объект приложения
 const app = express();
 
+//1 параметры маршрута получить из запроса
+app.get("/name/:nam/pet/:pet.:age", function (request, response) {
+    const pageName = request.params["name"];
+    const pagePet = request.params["pet"];
+    const pageAge = request.params["age"];
+    response.send(`Имя:${pageName} Питомец:${pagePet}.${pageAge}`);
+}); //localhost:3000/name/gera/pet/murka.25
+
+//2 middleware и методы use получить в глобальные объекты параметры маршрута и параметры строки запроса
 app.use("/name/:name/pet/:pet.:age", function (request, response,next) {
   console.log("Middleware:", request.params, request.query);
   pageName = request.params["name"];
@@ -16,7 +25,7 @@ app.use("/name/:name/pet/:pet.:age", function (request, response,next) {
 
 app.get("/name/:name/pet/:pet.:age", function (_, response) {
   response.send(`Запрошенный данные: ${pageName}, ${pageAge}, ${pagePet}, ${pageCity}, ${pageObl}`);
-});
+}); //localhost:3000/name/gera/pet/murka.25?city=cher&obl=vol
 
 // начинаем прослушивать подключения на 3000 порту
 app.listen(3000);
