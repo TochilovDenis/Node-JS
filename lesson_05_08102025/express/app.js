753cd651f51f931b2ccd4ab39943e7e7a18a024f
TochilovDenis <denis-tochilov@yandex.ru>
2025-10-09 18:57:02 +0300

    Маршрутизация в Express
    https://metanit.com/web/nodejs/4.3.php

--- lesson_05_08102025/express/app.js
+++ lesson_05_08102025/express/app.js
@@ -4,13 +4,23 @@ const fs = require("fs");
 // создаем объект приложения
 const app = express();
 
-app.use(express.static(__dirname + "/public")); 
+// обработка запроса по адресу /about
+app.get("/about", function(_, response){
+     
+    response.send("<h1>О сайте</h1>");
+});
  
-
-app.use("/", function(_, response){
+// обработка запроса по адресу /contact
+app.use("/contact", function(_, response){
+     
+    response.send("<h1>Контакты</h1>");
+});
+ 
+// обработка запроса к корню веб-сайта
+app.get("/", function(_, response){
+     
     response.send("<h1>Главная страница</h1>");
 });
-
 
 // начинаем прослушивать подключения на 3000 порту
 app.listen(3000);
