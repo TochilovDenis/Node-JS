e860ddc26848a804a32abaf441f7d505b112c0e7
TochilovDenis <denis-tochilov@yandex.ru>
2025-10-09 19:00:42 +0300

    Переадресация
    Для переадресации применяется метод redirect():
    
    redirect([status,] path)
    
    https://metanit.com/web/nodejs/4.13.php

--- lesson_05_08102025/express/app.js
+++ lesson_05_08102025/express/app.js
@@ -4,22 +4,8 @@ const fs = require("fs");
 // создаем объект приложения
 const app = express();
 
-// обработка запроса по адресу /about
-app.get("/about", function(_, response){
-     
-    response.send("<h1>О сайте</h1>");
-});
- 
-// обработка запроса по адресу /contact
-app.use("/contact", function(_, response){
-     
-    response.send("<h1>Контакты</h1>");
-});
- 
-// обработка запроса к корню веб-сайта
-app.get("/", function(_, response){
-     
-    response.send("<h1>Главная страница</h1>");
+app.use("/index",function (_, response) {
+  response.redirect("https://metanit.com")
 });
 
 // начинаем прослушивать подключения на 3000 порту
