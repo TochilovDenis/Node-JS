85a354f030cba69b62f5b1e3ee4f73ba03f58ecb
TochilovDenis <denis-tochilov@yandex.ru>
2025-10-09 19:27:30 +0300

    Переадресация

--- lesson_05_08102025/express/app.js
+++ lesson_05_08102025/express/app.js
@@ -4,27 +4,32 @@ const fs = require("fs");
 // создаем объект приложения
 const app = express();
 
-app.get("/aaa/bbb/ccc/eee/ggg/ttt/rrr/yyy",function (_, response) {
-  response.redirect("..")
+//localhost:3000/aaa/bbb/ccc/eee/ggg/ttt/rrr/yyy
+app.get("/aaa/bbb/ccc/eee/ggg/ttt/rrr/yyy", function (_, response) {
+  response.redirect("../../../../../")
+  //localhost:3000/aaa/bbb
 });
 
-
+//localhost:3000/aaa/bbb
 app.get("/aaa/bbb", function (_, response) {
- response.redirect("..")
+  response.redirect("../../ggg/ttt/rrr/yyy")
+  //localhost:3000/ggg/ttt
 });
 
-
+//localhost:3000/ggg/ttt
 app.get("/ggg/ttt/rrr/yyy", function (_, response) {
- response.redirect("..")
+  response.redirect("./yyy/aaa/bbb/ccc")
+  //localhost:3000/ggg/ttt/rrr/yyy/aaa/bbb/ccc
 });
- 
- 
+
+//localhost:3000/ggg/ttt/rrr/yyy/aaa/bbb/ccc
 app.get("/ggg/ttt/rrr/yyy/aaa/bbb/ccc", function (_, response) {
- response.redirect("..")
+  response.redirect("/")
+  //localhost:3000/aaa/bbb/ccc
 });
- 
-app.get("/", function(_, response){
-     response.send(`Запрошенный данные: ${name},${age},${pet},${city},${obl}`);
+
+app.get("/", function (_, response) {
+  response.send("<h1>Hello</h1>");
 });
 
 // начинаем прослушивать подключения на 3000 порту
