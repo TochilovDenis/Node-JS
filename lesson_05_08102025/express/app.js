5f3af2eb0e1812d9210c50670b1388625f2ad60e
TochilovDenis <denis-tochilov@yandex.ru>
2025-10-09 21:07:49 +0300

    Практическое задание.
    2 Используя middleware и методы use получить в глобальные объекты параметры маршрута и параметры строки запроса .
    "//localhost:3000/name/gera/pet/murka.25?city=cher&obl=vol"
    в конце написать метод get
    app.get("/", function(_, response){
        response.send(`Запрошенный данные: ${name},${age},${pet},${city},${obl}`);
    });

--- lesson_05_08102025/express/app.js
+++ lesson_05_08102025/express/app.js
@@ -4,11 +4,21 @@ const express = require("express");
 // создаем объект приложения
 const app = express();
 
-app.get("/name/:nam/pet/:pet.:age", function (request, response) {
-    const pageName = request.params["name"];
-    const pagePet = request.params["pet"];
-    const pageAge = request.params["age"];
-     response.send(`Имя:${pageName} Питомец:${pagePet}.${pageAge}`);
+pageName = ""; pagePet = ""; pageAge = ""; pageCity = ""; pageObl = "";
+app.get("/name/:name/pet/:pet.:age", function (request, response) {
+  console.log(1)
+  pageName = request.params["name"];
+  pagePet = request.params["pet"];
+  pageAge = request.params["age"];
+  pageCity = request.query.city;
+  pageObl = request.query.obl;
+  console.log(2)
+  response.redirect("/")
+});
+
+app.get("/", function (_, response) {
+  console.log(3)
+  response.send(`Запрошенный данные: ${pageName},${pageAge},${pagePet},${pageCity},${pageObl}`);
 });
 
 // начинаем прослушивать подключения на 3000 порту
