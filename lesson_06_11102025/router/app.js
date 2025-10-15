47aba74ff3fc3c61eaf915342120ba466c53d37b
TochilovDenis <denis-tochilov@yandex.ru>
2025-10-15 18:44:58 +0300

    Здесь у нас есть пять маршрутов, которые обрабатываются различными обработчиками. Но три из этих маршрутов начинаются с "/products" и условно относятся к некоторому функционалу по работе с товарами (просмотр списка товаров, просмотр одного товара по id и добавление товара). Объект Router позволяет связать подобный функционал в одно целое и упростить управление им. Например, перепишем предыдущий пример с использованием объекта Router
    https://metanit.com/web/nodejs/4.16.php

--- lesson_06_11102025/router/app.js
+++ lesson_06_11102025/router/app.js
@@ -1,18 +1,24 @@
 const express = require("express");
 const app = express();
  
-app.use("/about", function (_, response) {
-  response.send("О сайте");
-});
+// определяем Router
+const productRouter = express.Router();
  
-app.use("/products/create",function (request, response) {
+// определяем маршруты и их обработчики внутри роутера
+productRouter.use("/create", function(request, response){
   response.send("Добавление товара");
 });
-app.use("/products/:id",function (request, response) {
+productRouter.use("/:id", function(request, response){
   response.send(`Товар ${request.params.id}`);
 });
-app.use("/products/",function (request, response) {
+productRouter.use("/", function(request, response){
   response.send("Список товаров");
+});
+// сопоставляем роутер с конечной точкой "/products"
+app.use("/products", productRouter);
+ 
+app.use("/about", function (request, response) {
+  response.send("О сайте");
 });
  
 app.use("/", function (request, response) {
