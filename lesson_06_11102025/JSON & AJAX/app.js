14733a09bc9a88bbaba27d7468ec4a82ce32a30f
TochilovDenis <denis-tochilov@yandex.ru>
2025-10-15 18:52:31 +0300

    Далее определим в главном файле приложения - в app.js код, который бы принимал отправленные данные
    https://metanit.com/web/nodejs/4.6.php

---
+++ lesson_06_11102025/JSON & AJAX/app.js
@@ -0,0 +1,19 @@
+const express = require("express");
+   
+const app = express();
+// создаем парсер для данных в формате json
+const jsonParser = express.json();
+   
+app.post("/user", jsonParser, function (request, response) {
+    const user = request.body;
+    console.log(user);
+    if(!user) return response.sendStatus(400);
+    const responseText = `Your name: ${user.name}  Your age: ${user.age}`;
+    response.send(responseText); // отправляем ответ
+});
+   
+app.get("/", function(request, response){
+    response.sendFile(__dirname + "/index.html");
+});
+   
+app.listen(3000);
